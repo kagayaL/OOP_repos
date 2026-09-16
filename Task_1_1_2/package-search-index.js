a70ef44843703208e92cc7cebf537eb4cfb87588
kagayaL <kagayaL@users.noreply.github.com>
2026-09-16 19:14:12 +0000

    Deploying to gh-pages from @ kagayaL/OOP_repos@f1f10a9b58239cd42bd8b0930a39877be20e43b0 🚀

--- Task_1_1_2/package-search-index.js
+++ Task_1_1_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.kagaya.task_1_1_2"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.kagaya.task112"}];updateSearchResults();
