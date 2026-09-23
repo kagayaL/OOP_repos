edd67894abaa7c4e93d7c1e646e22776cc636bdd
Denis Masalov <d.masalov@g.nsu.ru>
2026-09-23 23:51:06 +0700

    Someone - abstract class

--- Task_1_1_2/src/main/java/ru/nsu/kagaya/task112/Someone.java
+++ Task_1_1_2/src/main/java/ru/nsu/kagaya/task112/Someone.java
@@ -3,9 +3,9 @@ package ru.nsu.kagaya.task112;
 /**
  * Класс, реализующий сущность, владеющую картами.
  */
-public class Someone {
-    Hand hand = new Hand();
-    String who;
+public abstract class Someone {
+    private final Hand hand = new Hand();
+    private final String name;
 
     /**
      * Конструктор, назначающий имя сущности.
@@ -13,7 +13,25 @@ public class Someone {
      * @param who имя сущности
      */
     public Someone(String who) {
-        this.who = who;
+        this.name = who;
+    }
+
+    /**
+     * Рука сущности.
+     *
+     * @return руку
+     */
+    public Hand getHand() {
+        return hand;
+    }
+
+    /**
+     * Имя сущности.
+     *
+     * @return имя сущности
+     */
+    public String getName() {
+        return name;
     }
 
     /**
@@ -22,24 +40,24 @@ public class Someone {
      * @param lastClosed булевая переменная для закрытой карты у дилера
      * @return строка со всеми картами
      */
-    public String formatCards(boolean lastClosed) {
+    public String toString(boolean lastClosed) {
         StringBuilder result = new StringBuilder();
-        result.append(who + "'s cards: [ ");
-        int lowAceCnt = hand.lowAceCnt;
-        for (int i = 0; i < hand.cardCount - 1; i++) {
+        result.append(name).append("'s cards: [ ");
+        int lowAceCnt = hand.getLowAceCnt();
+        for (int i = 0; i < hand.getCardCount() - 1; i++) {
             boolean isAceLow = !lastClosed
-                    && (hand.cards[i].rank == Card.Rank.ACE)
+                    && (hand.getCard(i).getRank() == Rank.ACE)
                     && (lowAceCnt-- > 0);
 
-            result.append(hand.cards[i].formatCard(isAceLow) + ", ");
+            result.append(hand.getCard(i).toString(isAceLow)).append(", ");
         }
         if (lastClosed) {
             result.append("<closed> ] => ?");
         } else {
-            boolean isAceLow = (hand.cards[hand.cardCount - 1].rank == Card.Rank.ACE)
+            boolean isAceLow = (hand.getCard(hand.getCardCount() - 1).getRank() == Rank.ACE)
                     && (lowAceCnt-- > 0);
-            result.append(hand.cards[hand.cardCount - 1].formatCard(isAceLow));
-            result.append(" ] => " + hand.currSum);
+            result.append(hand.getCard(hand.getCardCount() - 1).toString(isAceLow));
+            result.append(" ] => ").append(hand.getSum());
         }
         return result.toString();
     }
--- Task_1_1_2/src/test/java/ru/nsu/kagaya/task112/SomeoneTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/kagaya/task112/SomeoneTest.java
@@ -8,29 +8,29 @@ class SomeoneTest {
 
     @Test
     void twoAcePlayer() {
-        Someone player = new Someone("Player");
-        player.hand.getNewCard(new Card(Card.Suit.CLUBS, Card.Rank.ACE));
-        player.hand.getNewCard(new Card(Card.Suit.DIAMONDS, Card.Rank.ACE));
-        assertEquals(player.formatCards(false),
+        Player player = new Player();
+        player.getHand().addCard(new Card(Suit.CLUBS, Rank.ACE));
+        player.getHand().addCard(new Card(Suit.DIAMONDS, Rank.ACE));
+        assertEquals(player.toString(false),
                 "Player's cards: [ Ace Clubs ( 1 ), Ace Diamonds ( 11 ) ] => 12");
     }
 
     @Test
     void twoAceDealer() {
-        Someone player = new Someone("Dealer");
-        player.hand.getNewCard(new Card(Card.Suit.CLUBS, Card.Rank.ACE));
-        player.hand.getNewCard(new Card(Card.Suit.DIAMONDS, Card.Rank.ACE));
-        assertEquals(player.formatCards(true),
+        Dealer dealer = new Dealer();
+        dealer.getHand().addCard(new Card(Suit.CLUBS, Rank.ACE));
+        dealer.getHand().addCard(new Card(Suit.DIAMONDS, Rank.ACE));
+        assertEquals(dealer.toString(true),
                 "Dealer's cards: [ Ace Clubs ( 11 ), <closed> ] => ?");
     }
 
     @Test
     void defaultCard() {
-        Someone player = new Someone("Player");
-        player.hand.getNewCard(new Card(Card.Suit.CLUBS, Card.Rank.KING));
-        player.hand.getNewCard(new Card(Card.Suit.DIAMONDS, Card.Rank.EIGHT));
-        player.hand.getNewCard(new Card(Card.Suit.HEARTS, Card.Rank.TWO));
-        assertEquals(player.formatCards(false),
+        Player player = new Player();
+        player.getHand().addCard(new Card(Suit.CLUBS, Rank.KING));
+        player.getHand().addCard(new Card(Suit.DIAMONDS, Rank.EIGHT));
+        player.getHand().addCard(new Card(Suit.HEARTS, Rank.TWO));
+        assertEquals(player.toString(false),
                 "Player's cards: [ King Clubs ( 10 ), Eight Diamonds ( 8 ),"
                        + " Two Hearts ( 2 ) ] => 20");
     }
